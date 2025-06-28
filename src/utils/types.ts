a9c54ebe608de82dea502e72cf6189c1f96c58c8
Aleksandr <aleksdark13@gmail.com>
2025-06-28 15:05:16 +0300

    add modal

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -92,6 +92,7 @@ export type ModalNames =
     | 'addNewPackage'
     | 'addNewGiftBox'
     | 'productInfoModal'
+    | 'addNewOtherInvestmentModal'
     ;
 
 export const modalNames: ModalNames[] = [
@@ -109,7 +110,8 @@ export const modalNames: ModalNames[] = [
     'createCustomerDialog',
     'addNewPackage',
     'addNewGiftBox',
-    'productInfoModal'
+    'productInfoModal',
+    'addNewOtherInvestmentModal'
 ];
 
 export interface INotificationPanel {
